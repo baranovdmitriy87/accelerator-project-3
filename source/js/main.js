8a89224c17062daf62f959cff371bc4bba615b01
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2025-02-01 20:00:30 +0300

    Делает бургерное меню

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,28 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+
+
+const button = document.querySelector('.burger-menu__button');
+const menu = document.querySelector('.header__burger-menu');
+const overlay = document.querySelector('.overlay');
+const burgerNav = document.querySelector('.burger-menu__nav');
+
+  menu.addEventListener('click', function() {
+     button.classList.toggle('open');
+     menu.classList.toggle('is-active');
+     burgerNav.classList.toggle('open');
+})
+
+    onDocumentKeyDown();
+  const closeburgerToggle = () => {
+    overlay.classList.toggle('open');
+  };
+
+    const onDocumentKeyDown = () => {
+    document.addEventListener('keydown', function (evt) {
+      if (evt.key === 'Escape') {
+        closeburgerToggle();
+      }
+    });
+  };
