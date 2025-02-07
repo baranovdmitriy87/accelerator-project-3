8154e49a644d1cdd96160e1926acedac7120771a
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2025-02-08 00:22:49 +0300

    Реализует блок faq

--- source/js/main.js
+++ source/js/main.js
@@ -13,3 +13,26 @@ window.addEventListener('DOMContentLoaded', () => {
     initBurgerMenu();
   });
 });
+
+// ================================== \\
+
+document.addEventListener('DOMContentLoaded', () => {
+  const togglers = document.querySelectorAll('[data-toggle]');
+
+    togglers.forEach((btn) => {
+      btn.addEventListener('click', (e) => {
+         const selector = e.currentTarget.dataset.toggle
+         const block = document.querySelector(`${selector}`);
+        if (e.currentTarget.classList.contains('is-active')) {
+          block.style.maxHeight = '';
+        } else {
+          block.style.maxHeight = block.scrollHeight + 'px';
+        }
+
+         e.currentTarget.classList.toggle('is-active')
+      })
+    })
+	})
+
+
+
