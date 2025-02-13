1200a5417fbc60614306f6f04a558c1dd4d50117
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2025-02-14 00:58:49 +0300

    Реализует блок about

--- source/js/modules/init-faq-accordion.js
+++ source/js/modules/init-faq-accordion.js
@@ -1,18 +1,37 @@
 const togglers = document.querySelectorAll('[data-toggle]');
+const content = document.querySelector('.open')
 
 
 const initAccordion = () => {
+
+  // document.querySelectorAll('.faq-accordion__button').forEach(header => {
+  //   header.addEventListener('click', function () {
+  //     // Закрываем все открытые элементы
+  //     document.querySelectorAll('.faq-accordion__content').forEach(content => {
+  //       if (content !== this.nextElementSibling) {
+  //         content.classList.remove('is-active');
+  //       }
+  //     });
+
+  //     // Переключаем текущий элемент
+  //     this.nextElementSibling.classList.toggle('is-active');
+  //   });
+  // });
+
   togglers.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       const selector = e.currentTarget.dataset.toggle
       const block = document.querySelector(`${selector}`);
       if (e.currentTarget.classList.contains('is-active')) {
         block.style.maxHeight = '';
+        // content.style.display = 'none';
       } else {
+        // content.style.display = 'block';
         block.style.maxHeight = block.scrollHeight + 'px';
       }
 
       e.currentTarget.classList.toggle('is-active')
+      // content.classList.remove('open')
     })
   })
 }
