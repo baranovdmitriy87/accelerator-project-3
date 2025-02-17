0a761b55710a38d6ac368ca6f0a14df124ea1f28
baranovdmitriy87 <147241780+baranovdmitriy87@users.noreply.github.com>
2025-02-17 23:44:30 +0300

    Реализует слайдер с прогами по PP для мобильной и планшетной версий

--- source/js/modules/init-programs.js
+++ source/js/modules/init-programs.js
@@ -1,25 +1,21 @@
 import Swiper from 'swiper';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Scrollbar } from 'swiper/modules';
 import { isMobileDevice } from '../utils/is-mobile-device';
 
 const buttonNext = document.querySelector('.programs__next');
 const buttonPrev = document.querySelector('.programs__prev');
 
-Swiper.use([Navigation]);
+Swiper.use([Navigation, Scrollbar]);
+// Swiper.use([Scrollbar]);
 
 
 const initProgramsSlider = () => {
-  const setSwyperReview = new Swiper('.programs__slider', {
+  const setSwyperPrograms = new Swiper('.programs__slider', {
     direction: 'horizontal',
     allowTouchMove: isMobileDevice(),
-    // slidesPerView: 'auto',
+    slidesPerView: 'auto',
     spaceBetween: 30,
     speed: 300,
-
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: true,
-    },
 
     breakpoints: {
       320: {
@@ -32,21 +28,22 @@ const initProgramsSlider = () => {
         slidesPerView: 3,
         allowTouchMove: false,
         spaceBetween: 32,
-        // pagination: {
-        //   el: '.hero__swiper-pagination',
-        //   clickable: true,
-        // },
+
       },
     },
-
-
 
     navigation: {
       nextEl: buttonNext,
       prevEl: buttonPrev,
     },
+
+    scrollbar: {
+      el: ".programs__scrollbar",
+      draggable: true,
+      dragClass: 'custom-scrollbar-drag', // Класс для ползунка
+    },
   });
-  return setSwyperReview;
+  return setSwyperPrograms;
 };
 
 export { initProgramsSlider };
